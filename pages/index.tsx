e496ba0b1f359ad1b55e6492277c35ba6f988cc0
ivanaMesterLightIt <ivana.mester@lightit.io>
2022-12-07 16:20:09 -0300

    progress class 106 section 8

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,30 @@
-import { Typography } from "@mui/material";
+import { Card, CardContent, CardHeader, Grid } from "@mui/material";
 import { NextPage } from "next";
 import { Layout } from "../components/layouts";
 
 const HomePage: NextPage = () =>  {
   return (
-    <Layout>
-      <Typography variant="h1" color='primary'>Hola Mundo</Typography>
+    <Layout title="Home - OpenJira">
+      <Grid container spacing={ 2 }>
+        <Grid item xs={ 12 } sm={ 4 }>
+          <Card sx={{ height: 'calc(100vh - 100px)' }}>
+            <CardHeader title="Pendientes" />
+            <CardContent>
+              {/* Agregar lista de tareas */}
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={ 12 } sm={ 4 }>
+          <Card sx={{ height: 'calc(100vh - 100px)' }}>
+            <CardHeader title="En Progreso" />
+          </Card>
+        </Grid>
+        <Grid item xs={ 12 } sm={ 4 }>
+          <Card sx={{ height: 'calc(100vh - 100px)' }}>
+            <CardHeader title="Completadas" />
+          </Card>
+        </Grid>
+      </Grid>
     </Layout>
   )
 }
