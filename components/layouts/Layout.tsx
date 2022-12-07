054604c68a47ce0bfb219d14aa7d56b4f6752109
ivanaMesterLightIt <ivana.mester@lightit.io>
2022-12-07 13:24:18 -0300

    progress class 93 section 7

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -1,7 +1,7 @@
 import { Box } from "@mui/material";
 import Head from "next/head";
 import { FC, PropsWithChildren } from "react";
-import { Navbar } from "../ui";
+import { Navbar, Sidebar } from "../ui";
 
 interface Props {
     title?: string
@@ -14,6 +14,7 @@ export const Layout: FC<PropsWithChildren<Props>> = ({ title = 'OpenJira', child
                 <title>{ title }</title>
             </Head>
             <Navbar/>
+            <Sidebar/>
             <Box sx={{ padding: '10px 20px' }}>
                 { children }
             </Box>
