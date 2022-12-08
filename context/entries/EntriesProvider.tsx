40f8d1a889b97b644b49655801f4670195fd6ba5
ivanaMesterLightIt <ivana.mester@lightit.io>
2022-12-08 10:08:32 -0300

    progress class 108 section 8

--- context/entries/EntriesProvider.tsx
+++ context/entries/EntriesProvider.tsx
@@ -1,12 +1,33 @@
 import { FC, PropsWithChildren, useReducer } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import { Entry } from '../../interfaces';
 import { EntriesContext, entriesReducer } from './';
 
 export interface EntriesState {
-   entries: [];
+   entries: Entry[];
 }
 
 const Entries_INITIAL_STATE: EntriesState = {
-   entries: [],
+   entries: [
+      {
+         _id: uuidv4(),
+         description: 'This is a description 1',
+         status: 'pending',
+         createdAt: Date.now(),
+      },
+      {
+         _id: uuidv4(),
+         description: 'This is a description 2',
+         status: 'in-progress',
+         createdAt: Date.now() - 1000000,
+      },
+      {
+         _id: uuidv4(),
+         description: 'This is a description 3',
+         status: 'finished',
+         createdAt: Date.now() - 100000,
+      }
+   ],
 }
 
 export const EntriesProvider: FC<PropsWithChildren> = ({ children }) => {
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import { Card, CardContent, CardHeader, Grid } from "@mui/material";
 import { NextPage } from "next";
 import { Layout } from "../components/layouts";
+import { EntryList } from "../components/ui";
 
 const HomePage: NextPage = () =>  {
   return (
@@ -9,19 +10,19 @@ const HomePage: NextPage = () =>  {
         <Grid item xs={ 12 } sm={ 4 }>
           <Card sx={{ height: 'calc(100vh - 100px)' }}>
             <CardHeader title="Pendientes" />
-            <CardContent>
-              {/* Agregar lista de tareas */}
-            </CardContent>
+            <EntryList />
           </Card>
         </Grid>
         <Grid item xs={ 12 } sm={ 4 }>
           <Card sx={{ height: 'calc(100vh - 100px)' }}>
             <CardHeader title="En Progreso" />
+            <EntryList />
           </Card>
         </Grid>
         <Grid item xs={ 12 } sm={ 4 }>
           <Card sx={{ height: 'calc(100vh - 100px)' }}>
             <CardHeader title="Completadas" />
+            <EntryList />
           </Card>
         </Grid>
       </Grid>
