3a07896646a9c69422ef0801c6c5bc073a917a7b
ivanaMesterLightIt <ivana.mester@lightit.io>
2022-12-12 11:48:18 -0300

    progress class 132 section 9

--- context/entries/EntriesProvider.tsx
+++ context/entries/EntriesProvider.tsx
@@ -1,5 +1,5 @@
-import { FC, PropsWithChildren, useReducer } from 'react';
-import { v4 as uuidv4 } from 'uuid';
+import { FC, PropsWithChildren, useEffect, useReducer } from 'react';
+import { entriesApi } from '../../apis';
 import { Entry } from '../../interfaces';
 import { EntriesContext, entriesReducer } from './';
 
@@ -15,21 +15,25 @@ export const EntriesProvider: FC<PropsWithChildren> = ({ children }) => {
 
    const [state, dispatch] = useReducer( entriesReducer , Entries_INITIAL_STATE );
 
-   const addNewEntry = ( description: string ) => {
+   const addNewEntry = async ( description: string ) => {
 
-      const newEntry: Entry = {
-         _id: uuidv4(),
-         description,
-         createdAt: Date.now(),
-         status: 'pending'
-      }
+     const { data } = await entriesApi.post<Entry>('/entries', { description });
 
-      dispatch({ type: '[Entry] - Add-Entry', payload: newEntry})
+      dispatch({ type: '[Entry] - Add-Entry', payload: data})
    }
 
    const updateEntry = ( entry: Entry ) => {
       dispatch({ type: '[Entry] - Entry-Updated', payload: entry })
    }
+
+   const refreshEntries = async () => {
+      const { data } = await entriesApi.get<Entry[]>('/entries');
+      dispatch({ type: '[Entry] - Refresh-Data', payload: data })
+   }
+
+   useEffect(() => {
+      refreshEntries();
+   }, [])
 
    return (
       <EntriesContext.Provider value={{
