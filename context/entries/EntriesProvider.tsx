5197b97c94a2ec09566c7ea031bdbd80a83d37c0
ivanaMesterLightIt <ivana.mester@lightit.io>
2022-12-12 12:42:59 -0300

    progress class 135 section 9

--- context/entries/EntriesProvider.tsx
+++ context/entries/EntriesProvider.tsx
@@ -22,8 +22,14 @@ export const EntriesProvider: FC<PropsWithChildren> = ({ children }) => {
       dispatch({ type: '[Entry] - Add-Entry', payload: data})
    }
 
-   const updateEntry = ( entry: Entry ) => {
-      dispatch({ type: '[Entry] - Entry-Updated', payload: entry })
+   const updateEntry = async ( { _id, description, status }: Entry ) => {
+
+      try {
+         const { data } = await entriesApi.put<Entry>(`/entries/${_id}`, { description, status });
+         dispatch({ type: '[Entry] - Entry-Updated', payload: data })
+      } catch (error) {
+         
+      }
    }
 
    const refreshEntries = async () => {
